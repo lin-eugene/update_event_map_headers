57fccaed8a624f969570e3727f435ed08a289416
cwild <conor.wild@sky.com>
2021-05-07 10:50:26 +0100

    first

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# update_event_map_headers
+# Update Event Map Headers
+
+## Setup and enviroment
+Install anaconda and make sure the conda command is in your path
+
+run the following code
+
+```shell
+# Create, activate and install the dependencies in a new conda enviroment
+conda create -n update_event_map_headers
+conda activate update_event_map_headers
+conda install -c conda-forge gemmi fire
+
+# Download the repository
+git pull https://github.com/ConorFWild/update_event_map_headers.git
+
+# Run the code to update the headers
+python update_event_map_headers/update_event_map_headers.py path/to/pandda/dir
+```
+
+
---
+++ update_event_map_headers.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import fire
+import gemmi
+
+
+class Constants:
+    PANDDA_PROCESSED_DATASETS_DIR = "processed_datasets"
+
+
+def get_event_map_from_dataset_dir(dataset_dir: Path):
+    event_maps = dataset_dir.glob("*.ccp4")
+
+    return list(event_maps)
+
+
+def get_event_map_files(pandda_dir: Path):
+    processed_datasets_dir = pandda_dir / Constants.PANDDA_PROCESSED_DATASETS_DIR
+
+    dataset_dirs = processed_datasets_dir.glob("*")
+
+    event_map_files_nested = map(get_event_map_from_dataset_dir, dataset_dirs)
+
+    event_map_files = [event_map_file for event_map_files in event_map_files_nested for event_map_file in
+                       event_map_files]
+
+    return event_map_files
+
+
+def update_event_map_spacegroup(event_map_file: Path):
+    ccp4 = gemmi.read_ccp4_map(str(event_map_file))
+
+    ccp4.grid.spacegroup = gemmi.find_spacegroup_by_name("P 1")
+
+    ccp4.setup()
+
+    ccp4.update_ccp4_header(2, True)
+
+    ccp4.write_ccp4_map(str(event_map_file))
+    print(f"\tUpdated the event map at: {event_map_file}")
+
+
+def update_event_map_spacegroups(pandda_dir: str):
+    pandda_dir = Path(pandda_dir)
+    print(f"PanDDA dir is: {pandda_dir}")
+
+    event_map_files = get_event_map_files(pandda_dir)
+    print(f"Got {len(event_map_files)} event map files")
+
+    map(update_event_map_spacegroup, event_map_files)
+
+    print(f"Done!")
+
+
+if __name__ == "__main__":
+    fire.Fire(update_event_map_spacegroups)
